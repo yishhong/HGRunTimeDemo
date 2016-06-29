d7887d1abb6129dcb8d0fd59a332d07e4236f0a6
yishuhong <932977430@qq.com>
2016-06-29 15:35:28 +0800

    创建工程

---
+++ HGRunTimeDemo/HGRunTimeDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HGRunTimeDemo
+//
+//  Created by  易述宏 on 16/6/29.
+//  Copyright © 2016年 湖南同禾. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
